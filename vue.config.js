86235cba1b6a12a66864babb818d45b6542c7f3e
t w <tw@tdeMacBook-Pro.local>
2018-12-23 16:20:24 +0800

    初始化数据

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
+const appData = require('./data.json')
+
+const { seller, goods, ratings } = appData
+
 module.exports = {
+  devServer: {
+    // 模拟数据
+    before(app) {
+      app.get('/api/seller', function(req, res){
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+
+      app.get('/api/goods', function(req, res){
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+
+      app.get('/api/ratings', function(req, res){
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
+  },
   css: {
     loaderOptions: {
       stylus: {
