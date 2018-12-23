8ed042732fb980da9f5f1700e8aed6cabab02f0f
t w <tw@tdeMacBook-Pro.local>
2018-12-23 17:25:30 +0800

    头部组件

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,15 @@
+const webpack = require('webpack')
+const path = require('path')
 const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
 
-const { seller, goods, ratings } = appData
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 
 module.exports = {
-  devServer: {
-    // 模拟数据
-    before(app) {
-      app.get('/api/seller', function(req, res){
-        res.json({
-          errno: 0,
-          data: seller
-        })
-      })
-
-      app.get('/api/goods', function(req, res){
-        res.json({
-          errno: 0,
-          data: goods
-        })
-      })
-
-      app.get('/api/ratings', function(req, res){
-        res.json({
-          errno: 0,
-          data: ratings
-        })
-      })
-    }
-  },
   css: {
     loaderOptions: {
       stylus: {
@@ -43,5 +25,38 @@ module.exports = {
       postCompile: true,
       theme: true
     }
-  }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
+  },
+  chainWebpack(config) {
+    config.resolve.alias
+      .set('components', resolve('src/components'))
+      .set('common', resolve('src/common'))
+      .set('api', resolve('src/api'))
+
+    config.plugin('context')
+      .use(webpack.ContextReplacementPlugin,
+        [/moment[/\\]locale$/, /zh-cn/])
+  },
+  baseUrl: ''
 }
